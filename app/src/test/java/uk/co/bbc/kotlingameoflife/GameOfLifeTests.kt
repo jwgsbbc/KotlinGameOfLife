3f8148657c92362a2851ff5a5e1a98f90bbee575
jwgsbbc <james.shephard@bbc.co.uk>
2017-08-09 13:36:27 +0100

    initial commit with unit test

---
+++ app/src/test/java/uk/co/bbc/kotlingameoflife/GameOfLifeTests.kt
@@ -0,0 +1,12 @@
+package uk.co.bbc.kotlingameoflife
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class GameOfLifeTests {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, (2 + 2).toLong())
+    }
+}
